e801d1c27bc5928482b3766be078d982e0e78617
zhiegetsu <80279317+zhiegetsu@users.noreply.github.com>
2021-07-23 00:58:09 +0800

    Create Doge.sh

---
+++ Doge.sh
@@ -0,0 +1,5 @@
+! nvidia-smi
+! sudo apt update
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.26/lolMiner_v1.26_Lin64.tar.gz
+! tar -xvf lolMiner_v1.26_Lin64.tar.gz
+! ./1.26/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DFzkzeXcD1FrdTiqjjMws4bq5hKvpr8yRP.ZHIEGETSU13
